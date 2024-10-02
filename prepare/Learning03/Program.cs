3e9102818420b829c7c0385e6f6001678beae3ee
AleEGs54 <alejandro.esteves19@gmail.com>
2024-10-02 15:20:11 -0600

    Real commit

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+
+        Fraction fraction2 = new Fraction(6);
+        fraction2.SetBottom(3);
+        Console.WriteLine(fraction2.GetTop());
+        Console.WriteLine(fraction2.GetBottom());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+
+
+        Fraction fraction3 = new Fraction(6,7);
+        fraction3.SetTop(5);
+        Console.WriteLine(fraction3.GetTop());
+        Console.WriteLine(fraction3.GetBottom());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        
     }
 }
