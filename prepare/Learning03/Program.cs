2c769b824b05c1042ae730cecd536beb88fcb463
AleEGs54 <alejandro.esteves19@gmail.com>
2024-10-02 15:17:26 -0600

    Revert "Fractions" Reverting commit This reverts commit 152dd28a3ff1f2998c52cba4e912b0751755f7aa.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,26 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction fraction1 = new Fraction();
-        Console.WriteLine(fraction1.GetTop());
-        Console.WriteLine(fraction1.GetDecimalValue());
-
-
-        Fraction fraction2 = new Fraction(6);
-        fraction2.SetBottom(3);
-        Console.WriteLine(fraction2.GetTop());
-        Console.WriteLine(fraction2.GetBottom());
-        Console.WriteLine(fraction2.GetFractionString());
-        Console.WriteLine(fraction2.GetDecimalValue());
-
-
-
-        Fraction fraction3 = new Fraction(6,7);
-        fraction3.SetTop(5);
-        Console.WriteLine(fraction3.GetTop());
-        Console.WriteLine(fraction3.GetBottom());
-        Console.WriteLine(fraction3.GetDecimalValue());
-
-        
+        Console.WriteLine("Hello Learning03 World!");
     }
 }
