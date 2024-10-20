4b23f709d0ea48913ef25c34735f3997587dbffb
Alejandro Esteves <alejandro.esteves19@gmail.com>
2024-10-20 14:04:08 -0600

    Exceeding requirements

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,13 @@
+//Author: Alejandro Esteves
+//Exceeding Requirements: Keeping a log of how many times activities were performed and displaying a number when the user finishes the program.
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
+        int activitiesDone = 0;
         int userInput = 0;
 
         BreathingActivity breathingActivity = new BreathingActivity();
@@ -23,18 +27,24 @@ class Program
             if  (userInput == 1){
 
                 breathingActivity.Run();
+                activitiesDone++;
                 }
             else if  (userInput == 2){
                 reflectingActivity.Run();
+                activitiesDone++;
             }
             else if  (userInput == 3){
                 listingActivity.Run();
+                activitiesDone++;
             }
-            else {try{
-                
-            } catch{
-
-            }}
+            else if (userInput == 4)
+            {
+                string summary = (activitiesDone == 0)? $"You did {activitiesDone} activities today. You can do it even better, I know it!" : $"Thanks for choosing us! You did {activitiesDone} activities today. Good Job!";
+                Console.WriteLine(summary);
+            }
+            else {
+                Console.WriteLine("That's not a valid option");
+            }
         }
 
         
